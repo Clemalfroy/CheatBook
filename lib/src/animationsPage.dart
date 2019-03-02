d183ac7b81297bf067a41c79b0550f1d57fb0bd8
cmalfroy <cmalfroy@student.42.fr>
2019-03-03 00:03:11 +0100

    AnimationPage: Nice button animation

--- lib/src/animationsPage.dart
+++ lib/src/animationsPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimationsPage extends StatefulWidget {
@@ -62,8 +64,124 @@ class _AnimationsPageState extends State<AnimationsPage>
                       style: Theme.of(context).textTheme.title,
                     ),
                   ),
+                  Transform.translate(
+                    offset: Offset(-_animationController.value * 20,
+                        -_animationController.value * 20),
+                    child: Text("Translating text"),
+                  ),
+                  RadialMenu(),
                 ]);
           }),
+    );
+  }
+}
+
+class RadialMenu extends StatefulWidget {
+  @override
+  _RadialMenuState createState() => _RadialMenuState();
+}
+
+class _RadialMenuState extends State<RadialMenu>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 900),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RadialAnimation(controller: _animationController);
+  }
+}
+
+class RadialAnimation extends StatelessWidget {
+  RadialAnimation({
+    Key key,
+    this.controller,
+  })  : scale = Tween<double>(begin: 1.0, end: 0.0).animate(
+          CurvedAnimation(parent: controller, curve: Curves.easeIn),
+        ),
+        translation = Tween<double>(begin: 0.0, end: 70.0).animate(
+          CurvedAnimation(parent: controller, curve: Curves.easeIn),
+        ),
+        rotation = Tween<double>(begin: 0.0, end: 360.0).animate(
+          CurvedAnimation(
+              parent: controller,
+              curve: Interval(0.0, 0.9, curve: Curves.easeIn)),
+        ),
+        super(key: key);
+
+  final AnimationController controller;
+  final Animation<double> scale;
+  final Animation<double> translation;
+  final Animation<double> rotation;
+
+  _open() {
+    controller.forward();
+  }
+
+  _close() {
+    controller.reverse();
+  }
+
+  _buildButton(double angle, {Color color, IconData icon}) {
+    final double rad = angle * pi / 180;
+
+    return Transform(
+      transform: Matrix4.identity()
+        ..translate(
+          (translation.value) * cos(rad),
+          (translation.value) * sin(rad),
+        ),
+      child: FloatingActionButton(
+        heroTag: null,
+        onPressed: () {},
+        child: Icon(icon),
+        backgroundColor: color,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, builder) {
+        return Transform.rotate(
+          angle: rotation.value * pi / 180,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              _buildButton(0, icon: Icons.favorite, color: Colors.deepOrange),
+              _buildButton(270, icon: Icons.assessment, color: Colors.blue),
+              _buildButton(180, icon: Icons.title, color: Colors.green),
+              Transform.scale(
+                scale: scale.value - 1,
+                child: FloatingActionButton(
+                  heroTag: null,
+                  child: Icon(Icons.close),
+                  onPressed: _close,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+              Transform.scale(
+                scale: scale.value,
+                child: FloatingActionButton(
+                  heroTag: null,
+                  child: Icon(Icons.open_with),
+                  onPressed: _open,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
