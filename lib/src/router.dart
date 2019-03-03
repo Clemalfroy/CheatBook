a0165b83934833f01134a4682297c42079254b78
cmalfroy <cmalfroy@student.42.fr>
2019-03-03 13:56:04 +0100

    router: Add routes for FlarePage and PaintPage

--- lib/src/router.dart
+++ lib/src/router.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cheatbook/src/PainterPage.dart';
 import 'package:flutter_cheatbook/src/animationsPage.dart';
+import 'package:flutter_cheatbook/src/flareAnimationPage.dart';
 import 'package:flutter_cheatbook/src/home_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  "/" : (context) => HomePage(),
-  "/animationsPage" : (context) => AnimationsPage(),
-  "/animationsPagee" : (context) => AnimationsPage(),
-  "/animationsPageee" : (context) => AnimationsPage(),
-  "/animationsPageeee" : (context) => AnimationsPage(),
-  "/animationsPageeeee" : (context) => AnimationsPage(),
+  "/": (context) => HomePage(),
+  "/animationsPage": (context) => AnimationsPage(),
+  "/painterPage": (context) => PainterPage(),
+  "/flareAnimationPage": (context) => FlareAnimationPage(),
 };
