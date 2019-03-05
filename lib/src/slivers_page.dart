0b7ae0d1c5d5439411277aeb2852be3a9dab4e1d
cmalfroy <cmalfroy@student.42.fr>
2019-03-05 23:05:06 +0100

    Slivers Page: Add content to the flexible space

--- lib/src/slivers_page.dart
+++ lib/src/slivers_page.dart
@@ -11,17 +11,30 @@ class SliversPage extends StatelessWidget {
   }
 
   Widget _buildScrollable(BuildContext context) {
-    List<String> imageList = ['cmalfroy', 'anzouari', 'rghirell', 'curquiza'];
+    List<String> imageList = ['cmalfroy', 'cmalfroy', 'cmalfroy', 'cmalfroy'];
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
           title: Text('Slivers Page'),
+          expandedHeight: 200.0,
+          flexibleSpace: Align(
+            alignment: Alignment.bottomCenter,
+            child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              return FlexibleSpaceBar(
+                  background: Image.network(
+                "https://images.unsplash.com/photo-1542601098-3adb3baeb1ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5bb9a9747954cdd6eabe54e3688a407e&auto=format&fit=crop&w=500&q=60",
+                fit: BoxFit.cover,
+              ));
+            }),
+          ),
+          pinned: true,
         ),
         SliverPadding(
           padding: EdgeInsets.symmetric(vertical: 16.0),
           sliver: SliverToBoxAdapter(
             child: Container(
-              height: 150,
+              height: 150.0,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: imageList.length,
